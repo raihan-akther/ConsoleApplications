231e946da724a28790a4a427fb5c56b2955cf340
Raihan Akther <106593920+raihan-akther@users.noreply.github.com>
2022-08-11 21:33:37 +0600

    Update App.py

--- App.py
+++ App.py
@@ -1,49 +1,57 @@
-
-
-
-
 import os
 choice=''
-while choice not in ['Yes','No']:
-    choice=input('Do you want to file read or write? say yes or no: ')
 
-    if choice.lower()=='yes':
-        u=''
-        while u not in [1,2]:
+extension = ".txt"
+command = ''
+while command not in [1, 2]:
 
-                u=input('if you want file read -press 1 or if you want file write-press 2: ')
+    command = input('Enter 1 for read or 2 for write: ')
 
-                if u=='1':
-                        for root, dirs, files in os.walk(r"C:\Users\rhlen\Desktop\New folder\New folder (2)\peopleData"):
-                            file = input('Filename: ')
-                            extension = input('Extension: ')
-                            filename = ('{0}.{1}'.format(file, extension))
+    if command == '1':
+        # Taking name from user
+        name = input('Enter your name: ')
+        if not name:
+            continue
 
-                            if filename in files:
-                                file = open('{0}'.format(filename), "r")
-                                a=file.read()
-                                print(a)
-                                file.close
-                            else:
-                                print('file not found')
-                                print (os.path.join(root, name))
-                            break
+        filename = "_".join(name.lower().split(' '))  # Replacing space to lodash and doing lower case
 
+        # Making absolute path
+        full_filename = filename + extension
+        absolute_path = os.path.join(os.getcwd(), full_filename)
+        is_file_exist = os.path.isfile(absolute_path)
 
-                if u=='2':
-                    name=input('Name: ')
-                    age=input('Age: ')
-                    gender=input('Gender: ')
-                    work=input('Work: ')
-                    file= input('Filename: ')
-                    extension=input('Extension: ')
-                    filename=('{0}.{1}'.format(file,extension))
-                    file=open('{0}'.format(filename),"w")
-                    a=file.write(' name= {0} \n age={1} \n gender={2} \n work={3}'.format(name,age,gender,work))
-                    print(a)
-                    file.close
+        # Searching file and printing
+        if is_file_exist:
+            file = open('{0}'.format(absolute_path), "r")
+            name = file.readline()
+            age = file.readline()
+            gender = file.readline()
+            profession = file.readline()
+            print(' Name= {0} Age={1} Gender={2} Profession={3}'.format(name, age, gender, profession))
 
+            file.close()
+        else:
+            print('Name {} not found at path '.format(name))
+            print(absolute_path)
+            # break
 
-    if choice=='no':
-            print('sorry')
-    break
+    if command == '2':
+        # Taking user information
+        name = input('Please enter your name: ')
+        age = input('Age: ')
+        gender = input('Gender: ')
+        profession = input('Profession: ')
+
+        # Making absolute path
+        file_name = "_".join(name.lower().split(' '))
+        full_filename = file_name + extension
+        absolute_path = os.path.join(os.getcwd(), full_filename)
+
+        # Saving information to file
+        file = open(absolute_path, "w")
+        name_line = "\n"
+        file.write(name + name_line)
+        file.write(age + name_line)
+        file.write(gender + name_line)
+        file.write(profession + name_line)
+        file.close()
